54086f27fa8c6de70ec2040ba399c99c410368b7
novelot <novelot@163.com>
2015-09-03 15:46:34 +0800

    添加Menu

--- app/src/main/java/com/novelot/nover/MainActivity.java
+++ app/src/main/java/com/novelot/nover/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -102,5 +104,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /******************************************************************/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
